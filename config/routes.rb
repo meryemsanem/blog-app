c22111c5571ec0905319c0c88b174559deb13ce1
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-02 16:35:36 +0300

    Remove lines that cause repetition

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
