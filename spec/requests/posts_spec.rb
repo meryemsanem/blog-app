19cd41a899498543b221520464a9c2b2eb92a5ba
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-03 16:22:28 +0300

    Add test for posts controller's show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Posts of the user') 
     end
   end
+  describe 'GET /show' do
+    before :each do
+      get '/users/:user_id/posts/:id'
+    end
+
+    it 'checks the response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'checks if the show template is rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'checks if the response body includes the "Post with the user id" placeholder text' do
+      expect(response.body).to include('Post with the user id') 
+    end
+  end
 end
