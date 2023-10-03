39e76c747e7354c9833a806f4c653a08564a8db4
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-03 16:17:50 +0300

    Add test for users controller's show action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('All users') 
     end
   end
+describe 'GET /show' do
+    before :each do
+      get '/users/:id'
+    end
+
+    it 'checks the response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'checks if the show template is rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'checks if the response body includes the "User profile" placeholder text' do
+      expect(response.body).to include('User profile') 
+    end
+  end
 end
