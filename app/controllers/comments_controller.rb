d66b6aecbce5405b0d4985e2e2a9d6d63440c3e2
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-06 00:46:55 +0300

    Update add comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,10 @@
 class CommentsController < ApplicationController
-def new
-  @comment = Comment.new
-  @post = Post.find(params[:post_id])
-end
+  def new
+    @comment = Comment.new
+  end
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Comment.new(params.require(:comment).permit(:text))
     @comment.user = current_user
     @comment.post = Post.find(params[:post_id])
 
@@ -14,11 +13,5 @@ end
     else
       render :new
     end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:text)
   end
 end
