6f9baf76b9c8509e478e8f915bb6788022d1204b
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-06 00:51:40 +0300

    Update post model and create add-post functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,13 @@ class Post < ApplicationRecord
   validates :commentsCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likesCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-  after_create :update_user_posts_counter
+  after_create :update_user_posts_counter 
+  after_initialize :set_default
+
+  def set_default
+    self.likesCounter ||= 0
+    self.commentsCounter ||= 0
+  end
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
