0cafd1eefeb313065f459ab35c3f5c9cd0b34d72
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-09 17:34:53 +0300

    Write tests for post index page

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -18,4 +18,49 @@ RSpec.describe 'posts#index', type: :feature do
     ]
     visit user_posts_url(user_id: @user.id)
   end
+  describe '#index page' do
+    it 'can see the user profile picture.' do
+      expect(page).to have_css("img[src='#{@user.photo}']")
+    end
+    it 'can see the user username.' do
+      expect(page).to have_content(@user.name.to_s)
+    end
+    it 'can see the number of posts the user has written.' do
+      expect(page).to have_content(@user.posts_counter.to_s)
+    end
+    it 'can see posts title' do
+      @posts.each do |post|
+        expect(page).to have_content(post.title.to_s)
+      end
+    end
+    it 'can see some of the bodys post' do
+      @posts.each do |post|
+        expect(page).to have_content(post.text.to_s)
+      end
+    end
+    it 'can see the first comments on a post.' do
+      @posts.each do |post|
+        post.recent_comments.each do |comment|
+          expect(page).to have_content(comment.user.text)
+        end
+      end
+    end
+    it 'can see how many comments a post has' do
+      @posts.each do |post|
+        expect(page).to have_content(post.commentsCounter.to_s)
+      end
+    end
+    it 'can see how many likes a post has' do
+      @posts.each do |post|
+        expect(page).to have_content(post.likesCounter.to_s)
+      end
+    end
+  end
+  describe 'GET show/page' do
+    it 'redirects me to that posts show page, when I clicked on a post.' do
+      post = @posts.first
+      click_link(post.title)
+      expect(page).to have_current_path(user_post_path(post.author, post))
+    end
+  end
 end
