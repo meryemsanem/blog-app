3dcc0ad16ab6392462fb8b1c337678a30a06d861
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-11 12:57:56 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }, allow_nil: true
 
   def update_posts_counter
     update(posts_counter: posts.count)
