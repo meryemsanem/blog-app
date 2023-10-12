b158f5c3ff554b27255ea7f8feee69a67ba51978
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-12 11:22:38 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create ] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create, :new]
        resources :likes, only: [:create, :new] 
     end
