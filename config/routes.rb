bfc1265ceecc3ddb81f899a4433537030004e18f
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-13 10:42:40 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,19 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show] do
         resources :posts, only: [:index, :show] do
-          resources :comments, only: [:create, :destroy]
-          resources :likes, only: [:create]
+          resources :comments, only: [:create, :new, :destroy]
+          resources :likes, only: [:create, :new]
         end
       end
     end
   end
 
+  
   root to: "users#index"
-  resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:create, :new, :destroy]
-    resources :likes, only: [:create, :new]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :new, :destroy]
+      resources :likes, only: [:create, :new]
+    end
   end
 end
