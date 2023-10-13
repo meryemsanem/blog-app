9e468fc79c8a7641e29ddfd075f73335a040db21
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-13 11:16:50 +0300

    Create API endpoint to list the comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::CommentsController < ApplicationController
-end
+
+before_action :find_post, only: [:index, :create]
+
+      def index
+        @comments = @post.comments
+        render json: @comments
+      end
+
+      def create
+        @comment = @post.comments.new(comment_params)
+        @comment.user = current_user
+
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def find_post
+        @post = Post.find(params[:post_id])
+      end
+
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
+    end
