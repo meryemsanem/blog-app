ab16d8dbb69dc094f61d7ca72232c2daf7886c0f
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-10-13 12:15:28 +0300

    fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,30 +1,29 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :find_post, only: %i[index create]
 
-before_action :find_post, only: [:index, :create]
+  def index
+    @comments = @post.comments
+    render json: @comments
+  end
 
-      def index
-        @comments = @post.comments
-        render json: @comments
-      end
+  def create
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
 
-      def create
-        @comment = @post.comments.new(comment_params)
-        @comment.user = current_user
-
-        if @comment.save
-          render json: @comment, status: :created
-        else
-          render json: @comment.errors, status: :unprocessable_entity
-        end
-      end
-
-      private
-
-      def find_post
-        @post = Post.find(params[:post_id])
-      end
-
-      def comment_params
-        params.require(:comment).permit(:text)
-      end
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
